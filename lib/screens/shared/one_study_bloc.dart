35a5cfaafadf34cc73cb5233a122ee67447d946b
z33p <raphael00fellipe@gmail.com>
2021-06-27 20:11:53 -0300

    refac: bloc state managment with transform and on set functions

--- lib/screens/shared/one_study_bloc.dart
+++ lib/screens/shared/one_study_bloc.dart
@@ -1,14 +1,26 @@
 import 'dart:async';
 
 /// [OneStudyBloc] encapsulates getters and setters generating a single point of failure
-/// where the application state can only be modified by the same method.
+/// where the application state can only be modified by the same method [setValue].
 ///
-/// If you need to apply business rules in [setValue] you can implement this class in another class
-/// and override the behavior of this method.
+/// This class can be inherited in order to expand its functionality,
+/// but the focus is on writing the business rules in services
 class OneStudyBloc<T> {
   T _value;
+  T Function(T value)? _transformValue;
+  void Function(T value)? _onSetValue;
 
-  OneStudyBloc(this._value);
+  /// You can use the [transformValue] to apply a function to modify the value before [setValue].
+  /// Use  [onSetValue] only to apply business rules related to changes on screen,
+  /// any other business rules must be in the services.
+  OneStudyBloc(
+    this._value, {
+    void Function(T value)? onSetValue,
+    T Function(T value)? transformValue,
+  }) {
+    _onSetValue = onSetValue;
+    _transformValue = transformValue;
+  }
 
   T get value => _value;
 
@@ -16,6 +28,10 @@ class OneStudyBloc<T> {
   Stream<T> get stream => _valueStreamController.stream;
 
   void setValue(T value) {
+    if (_onSetValue != null) _onSetValue!(value);
+
+    if (_transformValue != null) value = _transformValue!(value);
+
     _value = value;
     _valueStreamController.add(value);
   }
